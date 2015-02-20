f26a18b3abd3d1fbaff8a457b6de5b68418e1b31
Matan Nassau <matan.nassau@gmail.com>
2015-02-20 17:46:45 -0500

    build.sh

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+project_dir="$(dirname "$0")"
+project_name="$(basename "$project_dir")"
+build_variant="debug"
+project_build_dir="$(realpath "${1:-"${project_dir}/../build_${project_name}"}")"
+project_build_variant_dir="$project_build_dir/$build_variant";
+
+echo "project_dir=\"$project_dir\"";
+echo "project_name=\"$project_name\"";
+echo "build_variant=\"$build_variant\"";
+echo "project_build_dir=\"$project_build_dir\"";
+echo "project_build_variant_dir=\"$project_build_variant_dir\"";
+
+if [ ! -d "$project_build_variant_dir" ]
+then
+  "${project_dir}"/rebuild.sh "$@"
+else
+  cd "$project_build_variant_dir" &&
+    make -j prefix=$HOME/usr/local/stow/"$project_name" V=1 check
+  if [ $? -eq 0 ];
+  then
+    say --voice=Vicki "build complete"
+  else
+    say --voice=Vicki "build failed"
+  fi
+fi
