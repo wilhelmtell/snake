9ccc919526190691ef2e2d06f129a2dd19c92668
Matan Nassau <matan.nassau@gmail.com>
2015-02-20 23:25:06 -0500

    build: fix verbosity handling

--- rebuild.sh
+++ rebuild.sh
@@ -7,40 +7,40 @@ error() {
   exit 1
 }
 
-[ "$V" = 2 ] && echo "Setting project_dir ..."
+[ "$V" -ge 2 ] && echo "Setting project_dir ..."
 project_dir="$(dirname "$0")"
-[ "$V" = 1 ] && echo "project_dir=\"${project_dir}\""
-[ "$V" = 2 ] && echo "Setting project_name ..."
+[ "$V" -ge 1 ] && echo "project_dir=\"${project_dir}\""
+[ "$V" -ge 2 ] && echo "Setting project_name ..."
 project_name="$(basename "$project_dir")"
-[ "$V" = 1 ] && echo "project_name=\"${project_name}\""
-[ "$V" = 2 ] && echo "Setting build_variant ..."
+[ "$V" -ge 1 ] && echo "project_name=\"${project_name}\""
+[ "$V" -ge 2 ] && echo "Setting build_variant ..."
 build_variant="${build_variant:-debug}"
-[ "$V" = 1 ] && echo "build_variant=\"${build_variant}\""
-[ "$V" = 2 ] && echo "Setting project_build_dir ..."
+[ "$V" -ge 1 ] && echo "build_variant=\"${build_variant}\""
+[ "$V" -ge 2 ] && echo "Setting project_build_dir ..."
 project_build_dir="$(realpath "${1:-"${project_dir}/../build_${project_name}"}")"
-[ "$V" = 1 ] && echo "project_build_dir=\"${project_build_dir}\""
-[ "$V" = 2 ] && echo "Setting project_build_variant_dir ..."
+[ "$V" -ge 1 ] && echo "project_build_dir=\"${project_build_dir}\""
+[ "$V" -ge 2 ] && echo "Setting project_build_variant_dir ..."
 project_build_variant_dir="$project_build_dir/$build_variant";
-[ "$V" = 1 ] && echo "project_build_variant_dir=\"${project_build_variant_dir}\""
-[ "$V" = 2 ] && echo "Setting CONFIG_SITE ..."
+[ "$V" -ge 1 ] && echo "project_build_variant_dir=\"${project_build_variant_dir}\""
+[ "$V" -ge 2 ] && echo "Setting CONFIG_SITE ..."
 CONFIG_SITE="$HOME/config.site.${build_variant}.clang"
-[ "$V" = 1 ] && echo "CONFIG_SITE=\"${CONFIG_SITE}\""
+[ "$V" -ge 1 ] && echo "CONFIG_SITE=\"${CONFIG_SITE}\""
 
 if [[ -d "$CONFIG_SITE" || ! -r "$CONFIG_SITE" ]];
 then
   error "CONFIG_SITE file unreadable or not found."
 fi
 
-[ "$V" = 1 ] && echo "Autoreconfing ..."
+[ "$V" -ge 1 ] && echo "Autoreconfing ..."
 autoreconf --install --force --warnings=all "$project_dir" ||
   error "autoreconf failed."
-[ "$V" = 1 ] && echo "Wiping build ..."
+[ "$V" -ge 1 ] && echo "Wiping build ..."
 rm -rf "$project_build_variant_dir" ||
   error "wiping build failed."
-[ "$V" = 2 ] && echo "Creating build directory ..."
+[ "$V" -ge 2 ] && echo "Creating build directory ..."
 mkdir -p "$project_build_variant_dir" ||
     error "build directory creation failed."
-[ "$V" = 1 ] && echo "Configuring build ..."
+[ "$V" -ge 1 ] && echo "Configuring build ..."
 cd "$project_build_variant_dir" &&
   CONFIG_SITE="${CONFIG_SITE}" "$project_dir"/configure --prefix=$HOME/usr/local ||
   error "Build configuration failed."
