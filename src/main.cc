b50f58bed48e3230823a078e33d912e341ebc15c
Matan Nassau <matan.nassau@gmail.com>
2015-02-21 12:42:17 -0500

    sdl sethint

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,12 @@
 #include "config.h"
 #include "sdl.hh"
+#include <SDL2/SDL.h>
+#include <iostream>
 
 int main(int /*argc*/, char * /*argv*/ []) {
   snk::sdl app;
+  if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+    std::cerr << "error: linear texturing filtering not enabled.\n";
+    return 1;
+  }
 }
