df09fcf52c47baef2ccb10356790504c62a9b056
Matan Nassau <matan.nassau@gmail.com>
2015-02-21 12:43:05 -0500

    SDL_CreateWindow

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,28 @@
 #include "config.h"
 #include "sdl.hh"
 #include <SDL2/SDL.h>
+#include <memory>
 #include <iostream>
+
+namespace snk {
+using window = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
+}
 
 int main(int /*argc*/, char * /*argv*/ []) {
   snk::sdl app;
   if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
     std::cerr << "error: linear texturing filtering not enabled.\n";
+    return 1;
+  }
+  snk::window window{SDL_CreateWindow("Snake",
+                                      SDL_WINDOWPOS_UNDEFINED,
+                                      SDL_WINDOWPOS_UNDEFINED,
+                                      640,
+                                      480,
+                                      SDL_WINDOW_SHOWN),
+                     SDL_DestroyWindow};
+  if(window == nullptr) {
+    std::cerr << "error: failed creating window.\n";
     return 1;
   }
 }
