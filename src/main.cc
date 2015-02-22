209155186106aba52175050ba4b3a02260c2982a
Matan Nassau <matan.nassau@gmail.com>
2015-02-22 13:50:25 -0500

    Revert "user can't move the berry"
    This reverts commit 2156dd2958bc6438faff8c1dcc65676ffbfb1bbd.

--- src/main.cc
+++ src/main.cc
@@ -58,7 +58,20 @@ int main(int /*argc*/, char * /*argv*/ []) {
   SDL_Rect snake = {random_x(), random_y(), BERRY_W, BERRY_H};
   while(true) {
     for(SDL_Event e; SDL_PollEvent(&e) != 0;) {
-      if(e.type == SDL_QUIT) return 0;
+      switch(e.type) {
+      case SDL_KEYDOWN:
+        if(e.key.keysym.sym == SDLK_LEFT && berry.x - 25 >= 0)
+          berry.x -= 25;
+        else if(e.key.keysym.sym == SDLK_RIGHT && berry.x + 25 < WINDOW_W)
+          berry.x += 25;
+        else if(e.key.keysym.sym == SDLK_UP && berry.y - 25 >= 0)
+          berry.y -= 25;
+        else if(e.key.keysym.sym == SDLK_DOWN && berry.y + 25 < WINDOW_H)
+          berry.y += 25;
+        break;
+      case SDL_QUIT:
+        return 0;
+      }
     }
     SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 0);
     SDL_RenderClear(renderer.get());
