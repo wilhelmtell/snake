b01c67ed95467ebc6d545018553d2f4f704e565e
Matan Nassau <matan.nassau@gmail.com>
2015-02-24 23:12:09 -0500

    berries at random positions

--- src/berry.cc
+++ src/berry.cc
@@ -2,21 +2,39 @@
 #include <SDL2/SDL.h>
 #include "game.hh"
 #include "renderer.hh"
+#include <random>
 
 namespace {
 int const BERRY_H = 10;
 int const BERRY_W = 10;
 unsigned int const BERRY_INTERVAL = 30000;  // ms
+
+int random_x(int max) {
+  std::random_device rd;
+  std::mt19937 gen{rd()};
+  std::uniform_int_distribution<> dist{0, max};
+  auto const x = dist(gen);
+  return x - (x % BERRY_W);
+}
+
+int random_y(int max) {
+  std::random_device rd;
+  std::mt19937 gen{rd()};
+  std::uniform_int_distribution<> dist{0, max};
+  auto const y = dist(gen);
+  return y - (y % BERRY_H);
+}
 }
 
 namespace snk {
 berry::berry()
-: rect{0, 0, BERRY_W, BERRY_H}, last_berry_time{SDL_GetTicks()} {}
+: rect{0, 0, BERRY_W, BERRY_H}
+, last_berry_time{SDL_GetTicks() - BERRY_INTERVAL} {}
 
-void berry::update(game const& /*g*/, SDL_Event const& /*e*/) {
+void berry::update(game const& g, SDL_Event const& /*e*/) {
   auto const t = SDL_GetTicks();
   if(t - last_berry_time > BERRY_INTERVAL) {
-    rect = {0, 0, BERRY_W, BERRY_H};
+    rect = {random_x(g.window_w()), random_y(g.window_h()), BERRY_W, BERRY_H};
     last_berry_time = t;
   }
 }
