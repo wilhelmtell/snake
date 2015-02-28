87d1d4f35b81a928efd0020b61b6d94305885236
Matan Nassau <matan.nassau@gmail.com>
2015-02-27 23:23:35 -0500

    snake: no need for unique_ptr

--- test/snake.cc
+++ test/snake.cc
@@ -1,10 +1,9 @@
 #include <catch/catch.hpp>
 #include "mock_snake_view.hh"
 #include "../src/snake_control.hh"
-#include <memory>
 
 TEST_CASE("snake movement") {
-  auto const view = std::make_unique<snk::test::mock_snake_view>();
-  snk::snake_control control{view.get()};
-  SECTION("snake is initially at origin") { REQUIRE(view->at_position(0, 0)); }
+  snk::test::mock_snake_view view;
+  snk::snake_control control{&view};
+  SECTION("snake is initially at origin") { REQUIRE(view.at_position(0, 0)); }
 }
