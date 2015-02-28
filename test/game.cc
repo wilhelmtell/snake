a9487285d37770e92ae85332a70396c7da20bd2a
Matan Nassau <matan.nassau@gmail.com>
2015-02-27 23:23:27 -0500

    game: no need for unique_ptr

--- test/game.cc
+++ test/game.cc
@@ -1,9 +1,8 @@
 #include <catch/catch.hpp>
 #include "../src/game_control.hh"
 #include "mock_game_view.hh"
-#include <memory>
 
 TEST_CASE("game") {
-  auto const view = std::make_unique<snk::test::mock_game_view>();
-  snk::game_control control{view.get()};
+  snk::test::mock_game_view view;
+  snk::game_control control{&view};
 }
