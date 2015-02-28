e5212bbca0d589932624acde519340c94d63e001
Matan Nassau <matan.nassau@gmail.com>
2015-02-28 12:33:16 -0500

    sdl_window_view has a window
    later i should rename window to sdl_window.

--- src/sdl_window_view.cc
+++ src/sdl_window_view.cc
@@ -1,4 +1,6 @@
 #include "sdl_window_view.hh"
 
 namespace snk {
+sdl_window_view::sdl_window_view(int const& width, int const& height)
+: w{width, height} {}
 }
--- src/sdl_window_view.hh
+++ src/sdl_window_view.hh
@@ -2,9 +2,15 @@
 #define SNK_SDL_WINDOW_VIEW_HH_
 
 #include "window_view.hh"
+#include "window.hh"
 
 namespace snk {
-struct sdl_window_view : window_view {};
+struct sdl_window_view : window_view {
+  sdl_window_view(int const& width, int const& height);
+
+private:
+  window w;
+};
 }
 
 #endif
