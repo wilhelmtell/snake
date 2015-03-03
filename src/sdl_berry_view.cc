0937245b4f05b50b6b4f9437e877866094d5932d
Matan Nassau <matan.nassau@gmail.com>
2015-03-03 15:17:14 -0500

    sdl_berry_view::controlled_by()

--- src/sdl_berry_view.cc
+++ src/sdl_berry_view.cc
@@ -1,3 +1,8 @@
 #include "sdl_berry_view.hh"
+#include "game_control.hh"
 
-namespace snk {}
+namespace snk {
+void sdl_berry_view::controlled_by(game_control* const control) {
+  this->control = control;
+}
+}
--- src/sdl_berry_view.hh
+++ src/sdl_berry_view.hh
@@ -2,9 +2,15 @@
 #define SNK_SDL_BERRY_VIEW_HH_
 
 #include "berry_view.hh"
+#include "game_control_fwd.hh"
 
 namespace snk {
-struct sdl_berry_view : berry_view {};
+struct sdl_berry_view : berry_view {
+  void controlled_by(game_control* const control);
+
+private:
+  game_control* control;
+};
 }
 
 #endif
