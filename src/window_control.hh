91befdfc9f238b27c1f1c00a96a38e3c490cb78e
Matan Nassau <matan.nassau@gmail.com>
2015-03-09 23:32:47 -0400

    window_control has input and output

--- src/window_control.hh
+++ src/window_control.hh
@@ -2,10 +2,17 @@
 #define SNK_WINDOW_CONTROL_HH_
 
 #include "abstract_factory_fwd.hh"
+#include <memory>
+#include "window_input.hh"
+#include "window_output.hh"
 
 namespace snk {
 struct window_control {
   explicit window_control(abstract_factory const& factory);
+
+private:
+  std::unique_ptr<window_input> in;
+  std::unique_ptr<window_output> out;
 };
 }
 
