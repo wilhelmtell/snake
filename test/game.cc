e840088f84cd1d81a6c060d3cb9c52864073ae11
Matan Nassau <matan.nassau@gmail.com>
2015-03-11 22:25:13 -0400

    rm silly game tests

--- test/game.cc
+++ test/game.cc
@@ -1,17 +1,2 @@
 #include <catch/catch.hpp>
-#include "mock_factory.hh"
-#include "quit_factory.hh"
 #include "../src/game_control.hh"
-
-TEST_CASE("game is initially on") {
-  snk::test::mock_factory factory;
-  snk::game_control control{factory};
-  // REQUIRE(!control.quit_requested());
-}
-
-TEST_CASE("upon quit, game is no longer on") {
-  snk::test::quit_factory factory;
-  snk::game_control control{factory};
-  // control.tick();
-  // REQUIRE(control.quit_requested());
-}
