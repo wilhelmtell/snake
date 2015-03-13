a11613c6be281c5a98ef3b8c6b208c5f84145580
Matan Nassau <matan.nassau@gmail.com>
2015-03-12 21:31:05 -0400

    test snake moving right won't move left

--- test/snake.cc
+++ test/snake.cc
@@ -2,6 +2,7 @@
 #include <memory>
 #include "../src/snake_control.hh"
 #include "mock_snake_output.hh"
+#include "event.hh"
 
 TEST_CASE("snake is initially at origin") {
   auto control_out = std::make_unique<snk::test::mock_snake_output>();
@@ -23,5 +24,21 @@ TEST_CASE("snake initially moves right") {
   control.update();
   control.draw();
   REQUIRE(out->x == 1);
+  REQUIRE(out->y == 0);
+}
+
+TEST_CASE("snake moving right won't move left") {
+  auto control_out = std::make_unique<snk::test::mock_snake_output>();
+  auto out = control_out.get();
+  snk::snake_control control{std::move(control_out)};
+  control.update();
+  control.draw();
+  control.update();
+  control.draw();
+  snk::event e{snk::event::keydown_left};
+  control.handle_event(e);
+  control.update();
+  control.draw();
+  REQUIRE(out->x == 2);
   REQUIRE(out->y == 0);
 }
