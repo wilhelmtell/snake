b0f20a916fd2666d0eb165d77de3c8bf41d18d10
Matan Nassau <matan.nassau@gmail.com>
2015-03-13 17:40:20 -0400

    snake_control: delegate ctor call

--- src/snake_control.cc
+++ src/snake_control.cc
@@ -4,7 +4,7 @@
 
 namespace snk {
 snake_control::snake_control(abstract_factory const& factory)
-: out{factory.make_snake_output()}, direction{-1}, x{0}, y{0}, w{25}, h{25} {}
+: snake_control{factory.make_snake_output()} {}
 
 snake_control::snake_control(std::unique_ptr<snake_output> out)
 : out{std::move(out)}, direction{-1}, x{0}, y{0}, w{25}, h{25} {}
