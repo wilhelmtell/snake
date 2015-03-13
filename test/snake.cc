9e7be4df78c585996042571f38751b046acbfe95
Matan Nassau <matan.nassau@gmail.com>
2015-03-12 21:28:14 -0400

    test snake originally moves right

--- test/snake.cc
+++ test/snake.cc
@@ -13,3 +13,15 @@ TEST_CASE("snake is initially at origin") {
   REQUIRE(out->x == 0);
   REQUIRE(out->y == 0);
 }
+
+TEST_CASE("snake initially moves right") {
+  auto control_out = std::make_unique<snk::test::mock_snake_output>();
+  auto out = control_out.get();
+  snk::snake_control control{std::move(control_out)};
+  control.update();
+  control.draw();
+  control.update();
+  control.draw();
+  REQUIRE(out->x == 1);
+  REQUIRE(out->y == 0);
+}
