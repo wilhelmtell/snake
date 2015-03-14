0c75962f5b48bacfb28175870e9ef2aff3861f2e
Matan Nassau <matan.nassau@gmail.com>
2015-03-13 23:53:16 -0400

    test snake moving up can move right

--- test/snake.cc
+++ test/snake.cc
@@ -210,3 +210,16 @@ TEST_CASE("snake moving up can move left") {
   control.draw();
   REQUIRE(out->pos == snk::position(0, 0));
 }
+
+TEST_CASE("snake moving up can move right") {
+  auto control_out = std::make_unique<snk::test::mock_snake_output>();
+  auto out = control_out.get();
+  snk::snake_control control{std::move(control_out), snk::position{1, 1}};
+  control.handle_event(snk::event::keydown_up);
+  control.update();
+  control.draw();
+  control.handle_event(snk::event::keydown_right);
+  control.update();
+  control.draw();
+  REQUIRE(out->pos == snk::position(2, 0));
+}
