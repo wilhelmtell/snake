01a1005a9071d26524c018bdbe3cad4c1488b85c
Matan Nassau <matan.nassau@gmail.com>
2015-03-13 23:11:43 -0400

    test snake moving up won't move down

--- test/snake.cc
+++ test/snake.cc
@@ -107,6 +107,19 @@ TEST_CASE("snake moving down won't move up") {
   REQUIRE(out->pos == snk::position(2, 4));  // snake moved to the left again
 }
 
+TEST_CASE("snake moving up won't move down") {
+  auto control_out = std::make_unique<snk::test::mock_snake_output>();
+  auto out = control_out.get();
+  snk::snake_control control{std::move(control_out), snk::position{2, 2}};
+  control.handle_event(snk::event::keydown_up);
+  control.update();
+  control.draw();
+  control.handle_event(snk::event::keydown_down);
+  control.update();
+  control.draw();
+  REQUIRE(out->pos == snk::position(2, 0));  // snake moved to the left again
+}
+
 TEST_CASE("snake moving right can move down") {
   auto control_out = std::make_unique<snk::test::mock_snake_output>();
   auto out = control_out.get();
