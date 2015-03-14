60152bd0263bb074c0ba6f10506d3661e9c73190
Matan Nassau <matan.nassau@gmail.com>
2015-03-14 00:24:29 -0400

    test snake down key when moving down is a no-op

--- test/snake.cc
+++ test/snake.cc
@@ -224,6 +224,19 @@ TEST_CASE("snake moving down can move right") {
   REQUIRE(out->pos == snk::position(2, 2));
 }
 
+TEST_CASE("moving down is a no-op when snake is already moving down") {
+  auto control_out = std::make_unique<snk::test::mock_snake_output>();
+  auto out = control_out.get();
+  snk::snake_control control{std::move(control_out), snk::position{2, 2}};
+  control.handle_event(snk::event::keydown_down);
+  control.update();
+  control.draw();
+  control.handle_event(snk::event::keydown_down);
+  control.update();
+  control.draw();
+  REQUIRE(out->pos == snk::position(2, 4));
+}
+
 TEST_CASE("snake moving up can move left") {
   auto control_out = std::make_unique<snk::test::mock_snake_output>();
   auto out = control_out.get();
