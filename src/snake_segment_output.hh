dedf909f4593308af730e3a6c51824b3e76a3574
Matan Nassau <matan.nassau@gmail.com>
2015-03-20 02:24:01 -0400

    snake_segment_output

---
+++ src/snake_segment_output.hh
@@ -0,0 +1,10 @@
+#ifndef SNK_SNAKE_SEGMENT_OUTPUT_HH_
+#define SNK_SNAKE_SEGMENT_OUTPUT_HH_
+
+namespace snk {
+struct snake_segment_output {
+  virtual ~snake_segment_output() {}
+};
+}
+
+#endif
---
+++ src/snake_segment_output_fwd.hh
@@ -0,0 +1,8 @@
+#ifndef SNK_SNAKE_SEGMENT_OUTPUT_FWD_HH_
+#define SNK_SNAKE_SEGMENT_OUTPUT_FWD_HH_
+
+namespace snk {
+struct snake_segment_output;
+}
+
+#endif
