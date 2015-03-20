394aed75be30e8fce4c901651fdaee2e1583eb3a
Matan Nassau <matan.nassau@gmail.com>
2015-03-20 01:31:13 -0400

    rectangle has coordinates

--- src/rectangle.cc
+++ src/rectangle.cc
@@ -1,7 +1,13 @@
 #include "rectangle.hh"
+#include <utility>
 #include "width.hh"
 #include "height.hh"
+#include "position.hh"
 
 namespace snk {
-rectangle::rectangle(width w, height h) : w{w.get()}, h{h.get()} {}
+rectangle::rectangle(width w, height h)
+: rectangle{std::move(w), std::move(h), position{0, 0}} {}
+
+rectangle::rectangle(width w, height h, position const& p)
+: w{w.get()}, h{h.get()}, x{p.x}, y{p.y} {}
 }
--- src/rectangle.hh
+++ src/rectangle.hh
@@ -3,15 +3,19 @@
 
 #include "width.hh"
 #include "height.hh"
+#include "position_fwd.hh"
 
 namespace snk {
 struct rectangle {
   rectangle() = default;
   rectangle(width w, height h);
+  rectangle(width w, height h, position const& p);
 
 public:
   int w;
   int h;
+  int x;
+  int y;
 };
 }
 
