fe768d6e2628da28fb00dd565eb80dc996038daa
Matan Nassau <matan.nassau@gmail.com>
2015-03-20 23:07:28 -0400

    snake_segment handles move events

--- src/snake_segment_control.hh
+++ src/snake_segment_control.hh
@@ -6,13 +6,14 @@
 #include "snake_segment_output.hh"
 #include "rectangle.hh"
 #include "event_fwd.hh"
+#include "direction.hh"
 
 namespace snk {
 struct snake_segment_control {
   explicit snake_segment_control(abstract_factory* factory);
   explicit snake_segment_control(std::unique_ptr<snake_segment_output> out);
   snake_segment_control(std::unique_ptr<snake_segment_output> out,
-                        rectangle rect);
+                        rectangle rect, direction move_requested);
 
   void handle_event(event const& e);
   void update();
@@ -23,6 +24,8 @@ struct snake_segment_control {
 public:
   std::unique_ptr<snake_segment_output> out;
   rectangle rect;
+  direction move_requested;
+  direction move_to;
 };
 }
 
