69cb3cbd302b2297976ec44e4f1430ff037fecae
Matan Nassau <matan.nassau@gmail.com>
2015-03-22 19:37:18 -0400

    direction are_opposite(): ... or vice-versa
    handle left<->right as well as right<->left. ditto for up/down.

--- src/direction.cc
+++ src/direction.cc
@@ -3,6 +3,8 @@
 namespace snk {
 bool are_opposite(direction const& a, direction const& b) {
   return (a == direction::left && b == direction::right)
-         || (a == direction::up && b == direction::down);
+         || (b == direction::left && a == direction::right)
+         || (a == direction::up && b == direction::down)
+         || (b == direction::up && a == direction::down);
 }
 }
