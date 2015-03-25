52cbf958810e89bb7a99d570f18b7882d5074762
Matan Nassau <matan.nassau@gmail.com>
2015-03-25 19:14:13 -0400

    aesthetics
    empty lines between function definitions.

--- test/mock_game_output.cc
+++ test/mock_game_output.cc
@@ -8,7 +8,9 @@ mock_game_output::mock_game_output(rectangle bounds)
 : bounding_rect{std::move(bounds)} {}
 
 void mock_game_output::clear(int /*r*/, int /*g*/, int /*b*/, int /*a*/) {}
+
 void mock_game_output::present() {}
+
 rectangle mock_game_output::bounds() const { return bounding_rect; }
 }
 }
