560c8cd3fb42a882fc738d134579b511b723e511
Matan Nassau <matan.nassau@gmail.com>
2015-03-27 15:58:24 -0400

    use std::chrono for timing in the main loop

--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,31 @@
 #include "config.h"
 #include <SDL2/SDL.h>
 #include "sdl_app.hh"
+#include <chrono>
 
 namespace {
-auto const milliseconds_per_frame = 1.0 / 60 * 1000;
+using clock = std::chrono::system_clock;
+auto const target_frames_per_second = 60;
+auto const time_per_frame = clock::duration{static_cast<clock::rep>(
+  1.0 / target_frames_per_second * clock::period::den)};
 }
 
 int main(int /*argc*/, char* /*argv*/ []) {
   snk::sdl_app app;
-  auto previous_timestamp = SDL_GetTicks();
-  auto lag = 0.0;
+  auto previous_timestamp = clock::now();
+  clock::duration lag{0};
   while(true) {
-    auto const current_timestamp = SDL_GetTicks();
-    auto const elapsed = current_timestamp - previous_timestamp;
+    auto const current_timestamp = clock::now();
+    auto const elapsed_time = current_timestamp - previous_timestamp;
     previous_timestamp = current_timestamp;
-    lag += elapsed;
+    lag += elapsed_time;
     for(SDL_Event e; SDL_PollEvent(&e) != 0;) {
       if(e.type == SDL_QUIT) return 0;
       app.handle_event(e);
     }
-    while(lag >= milliseconds_per_frame) {
+    while(lag >= time_per_frame) {
       app.update();
-      lag -= milliseconds_per_frame;
+      lag -= time_per_frame;
     }
     app.draw();
   }
